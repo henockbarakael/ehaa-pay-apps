27fd07da97715c5f209245171eb7f021ef81086f
Henock BARAKAEL <barahenock@gmail.com>
2023-11-16 15:01:47 +0100

    new design Z

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -94,30 +94,6 @@
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="blog.html" tabindex="-1">
-                            <div class="avatar avatar-40 rounded icon"><i class="bi bi-newspaper"></i></div>
-                            <div class="col">Blogs</div>
-                            <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                        </a>
-                    </li>
-
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="style.html" tabindex="-1">
-                            <div class="avatar avatar-40 rounded icon"><i class="bi bi-palette"></i></div>
-                            <div class="col">Style <i class="bi bi-star-fill text-warning small"></i></div>
-                            <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                        </a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="pages.html" tabindex="-1">
-                            <div class="avatar avatar-40 rounded icon"><i class="bi bi-file-earmark-text"></i></div>
-                            <div class="col">Pages <span class="badge bg-info fw-light">new</span></div>
-                            <div class="arrow"><i class="bi bi-chevron-right"></i></div>
-                        </a>
-                    </li>
-                    <li class="nav-item">
                         <a class="nav-link" href="signin.html" tabindex="-1">
                             <div class="avatar avatar-40 rounded icon"><i class="bi bi-box-arrow-right"></i></div>
                             <div class="col">Logout</div>
